c6446dfa90670841b22fc54208f6e6cf25999114
Noé Falzon <1331434+Castux@users.noreply.github.com>
2023-11-14 00:22:06 +0200

    Replaced config with example

--- night_lamp/config-example.lua
+++ night_lamp/config-example.lua
@@ -1,24 +1,24 @@
 local images = dofile "images.lua"
 
 return {
-	ssid = "XXX",
-	password = "XXX",
+	ssid = "SSID",
+	password = "PASSWORD",
 	timezone = 2,
 
 	-- RGB led
-	pin_r = 5,
-	pin_g = 6,
-	pin_b = 7,
+	pin_r = 3,
+	pin_g = 1,
+	pin_b = 2,
 
 	-- I2C display
-	scl = 1,
-	sda = 2,
+	scl = 6,
+	sda = 5,
 	sla = 0x3c,
 
 	times = {
 --		{from = "00:00", image = images.sleeping, color = {0.7,0,0}},
 		{from = "07:00", image = images.waking, color = {1,0.5,0}},
-		{from = "07:30", image = images.awake, color = {0.2,1,0}},
+		{from = "07:45", image = images.awake, color = {0.2,1,0}},
 		{from = "20:00", image = images.sleeping, color = {0.7,0,0}}
 	}
 }
